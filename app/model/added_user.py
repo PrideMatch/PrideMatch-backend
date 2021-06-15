8e9c341093e802540191f814825e4787073fde5f
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-15 21:25:01 +0200

    add added_user model

---
+++ app/model/added_user.py
@@ -0,0 +1,11 @@
+from app import db
+
+class AddedUser(db.Model):
+    def __init__(self, id, user_id, followed_user_id):
+        self.id = id
+        self.user_id = user_id
+        self.followed_user_id = followed_user_id
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    followed_user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
