ccd25467cf2e796ae8a4f8520c3f6cc3e4d8e542
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-15 22:59:37 +0200

    change connection string

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,5 @@ from flask_sqlalchemy import SQLAlchemy
 import secrets
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{0}:{1}@{2}/{3}'.format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{0}:{1}@{2}/{3}'.format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
 db = SQLAlchemy(app)
