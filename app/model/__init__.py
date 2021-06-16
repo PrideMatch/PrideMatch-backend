48235fafbfde6098ea62319ae51ae6f190ce59c5
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-16 20:27:42 +0200

    add model package init

---
+++ app/model/__init__.py
@@ -0,0 +1,5 @@
+from app.model.user import User
+from app.model.user_game import UserGame
+from app.model.teammate import Teammate
+from app.model.socials import Socials
+from app.model.added_user import AddedUser
