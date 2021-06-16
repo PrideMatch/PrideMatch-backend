0ee863ac6b02b63aa3eb3c4a816df29a69c76db7
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-16 20:30:43 +0200

    add views import to app package init

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,5 @@ import server_secrets
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{0}:{1}@{2}/{3}'.format(server_secrets.dbuser, server_secrets.dbpass, server_secrets.dbhost, server_secrets.dbname)
 db = SQLAlchemy(app)
+
+from app import views
