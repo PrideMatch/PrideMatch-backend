9b35ef3fc2526da8498ae2545b932bbda7a5123e
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-17 04:41:36 +0200

    add new model imports to init file

--- app/model/__init__.py
+++ app/model/__init__.py
@@ -3,3 +3,5 @@ from app.model.user_game import UserGame
 from app.model.teammate import Teammate
 from app.model.socials import Socials
 from app.model.added_user import AddedUser
+from app.model.interest import Interest
+from app.model.ignored_user import IgnoredUser
