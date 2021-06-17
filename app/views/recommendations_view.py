819aeacb5bb9010cc75746e719d27a4d6a1d1bf7
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-17 04:02:08 +0200

    add teammates mathing

--- app/views/recommendations_view.py
+++ app/views/recommendations_view.py
@@ -10,16 +10,43 @@ from app.model import User, AddedUser, Teammate
 def follow():
     user_id = request.args.get('user_id')
     data = request.get_json()
-    followed_user = data.get('added_user_id')
+    followed_user_id = data.get('added_user_id')
 
-    if not user_id or not data or not followed_user:
+    if not user_id or not data or not followed_user_id:
         make_response('Bad request', 400)
 
-    added_user = AddedUser(id=str(uuid.uuid4()), user_id=user_id, followed_user_id=followed_user)
+    added_users = AddedUser.query.filter_by(user_id=followed_user_id).all()
+
+    # if both users follow each other add them as teammates
+    if added_users:
+        for u in added_users:
+            if u.followed_user_id == user_id:
+                
+                # add teammate entry for both of the users
+                follower_teammate = Teammate(id=str(uuid.uuid4()), user_id=user_id, teammate_id=followed_user_id)
+                followed_teammate = Teammate(id=str(uuid.uuid4()), user_id=followed_user_id, teammate_id=user_id)
+
+                # remove added_user entry from followed user's list
+                added_user_entry = AddedUser.query.filter_by(followed_user_id=user_id).first()
+                db.session.delete(added_user_entry)
+
+                # add teammate entries to database
+                db.session.add(follower_teammate)
+                db.session.add(followed_teammate)
+                db.session.commit()
+
+                response = make_response('Users are now teammates', 200)
+                response.headers['X-Teammates']='true'
+                return response
+
+    # if followed user didnt add current user before, add added_user entry to the database
+    added_user = AddedUser(id=str(uuid.uuid4()), user_id=user_id, followed_user_id=followed_user_id)
     
     db.session.add(added_user)
     db.session.commit()
 
-    return make_response('User followed', 200)
+    response = make_response('Users are now teammates', 200)
+    response.headers['X-Teammates']='false'
+    return response
     
 
