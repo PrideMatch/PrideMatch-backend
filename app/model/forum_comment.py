3259bf2e1aec28cceb59302842945ef48d4437b2
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-18 01:58:10 +0200

    add init methods

--- app/model/forum_comment.py
+++ app/model/forum_comment.py
@@ -1,6 +1,14 @@
 from app import db
 
 class ForumComment(db.Model):
+    def __init__(self, id, text, creation_time, author, thread, reply_to=''):
+        self.id=id
+        self.author=author
+        self.text=text
+        self.creation_time=creation_time
+        self.thread=thread
+        self.reply_to=reply_to
+        
     id = db.Column(db.String(50), primary_key=True)
     text = db.Column(db.Text, nullable=False)
     creation_time = db.Column(db.DateTime(), nullable=False)
