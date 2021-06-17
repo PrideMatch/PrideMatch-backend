070305a825a7e60f539c220fc7dba3c10d39df1c
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-17 13:28:15 +0200

    fix ignored user model

--- app/model/ignored_user.py
+++ app/model/ignored_user.py
@@ -1,6 +1,11 @@
 from app import db
 
 class IgnoredUser(db.Model):
+    def __init__(self, id, user_id, ignored_user_id):
+        self.id = id
+        self.user_id = user_id
+        self.ignored_user_id = ignored_user_id
+
     id = db.Column(db.String(50), primary_key=True)
-    user_id = db.Column(db.String(50), db.ForeignKey('user.id'))
-    ignored_user_id = db.Column(db.String(50), db.ForeignKey('user.id'))
+    user_id = db.Column(db.String(50), db.ForeignKey('user.id'), nullable=False)
+    ignored_user_id = db.Column(db.String(50), nullable=False)
