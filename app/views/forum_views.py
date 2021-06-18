a06316e2b6efea340259de62ff0de4b11eb2a01b
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-18 04:11:16 +0200

    add add_thread_to_section endpoint

--- app/views/forum_views.py
+++ app/views/forum_views.py
@@ -1,3 +1,4 @@
+import uuid
 from app import app, db
 from app.model import ForumThread
 from flask.helpers import make_response
@@ -5,7 +6,7 @@ from flask import json, jsonify, request
 from app.views.json_parser import user_to_json
 from app.views.authorization import token_required
 
-@app.route('/forum/threads')
+@app.route('/forum/threads', methods=['GET'])
 @token_required
 def get_threads_from_section():
     section_name = request.args.get('forum_section')
@@ -17,6 +18,24 @@ def get_threads_from_section():
     forum_threads = sorted(forum_threads, key=compare_items_by_date)
 
     return make_response(jsonify(forum_threads), 200)
+
+@app.route('forum/threads', methods=['POST'])
+@token_required
+def add_thread_to_section():
+    section_name = request.args.get('forum_section')
+    data = request.get_json()
+
+    if not section_name or not data:
+        return make_response("Bad request", 400)
+    
+    forum_thread = ForumThread(id=str(uuid.uuid4()), author=data.get('author'), title=data.get('title'), creation_time=data.get('creation_time'),
+    text=data.get('text'), forum_section=section_name)
+
+    db.session.add(forum_thread)
+    db.session.commit()
+
+    return make_response('Forum thread created', 201)
+
 
 def compare_items_by_date(item1, item2):
     if item1.creation_time < item2.creation_time:
