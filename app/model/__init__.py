fcb7f56863a55899dd2a20ca76df4eb1904ac2d8
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-18 21:25:17 +0200

    add unread follow import

--- app/model/__init__.py
+++ app/model/__init__.py
@@ -7,3 +7,4 @@ from app.model.interest import Interest
 from app.model.ignored_user import IgnoredUser
 from app.model.forum_thread import ForumThread
 from app.model.forum_comment import ForumComment
+from app.model.unread_follow import UnreadFollow
