76597beb8b374f208e5fe2d6d701a4b3a1de36ad
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-18 19:46:04 +0200

    add unread follow model

---
+++ app/model/unread_follow.py
@@ -0,0 +1,9 @@
+from app import db
+
+class UnreadFollow(db.Model):
+    def __init__(self, id, followed_by):
+        self.id = id
+        self.followed_by = followed_by
+
+    id = db.Column(db.String(50), primary_key=True)
+    followed_by = db.Column(db.String(50), db.ForeignKey('user.id'))
