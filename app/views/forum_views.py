47f48dfe580552004967dbe6341207d24b1eea36
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-18 04:17:06 +0200

    add get_comments_from_thread endpoint

--- app/views/forum_views.py
+++ app/views/forum_views.py
@@ -1,6 +1,6 @@
 import uuid
 from app import app, db
-from app.model import ForumThread
+from app.model import ForumThread, ForumComment
 from flask.helpers import make_response
 from flask import json, jsonify, request
 from app.views.json_parser import user_to_json
@@ -35,6 +35,19 @@ def add_thread_to_section():
     db.session.commit()
 
     return make_response('Forum thread created', 201)
+
+@app.route('forum/comments', methods=['GET'])
+@token_required
+def get_comments_from_thread():
+    thread_id = request.args.get('thread_id')
+
+    if not thread_id:
+        return make_response("Bad request", 400)
+
+    comments = ForumComment.query.filter_by(thread=thread_id).all()
+    comments = sorted(comments, key=compare_items_by_date, reverse=True)
+
+    return make_response(jsonify(comments), 200)
 
 
 def compare_items_by_date(item1, item2):
