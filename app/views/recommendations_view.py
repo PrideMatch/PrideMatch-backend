6cba771dbe155efc3ef493f1a1b16f16617f8402
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-19 01:15:49 +0200

    fix interest recommendations endpoint

--- app/views/recommendations_view.py
+++ app/views/recommendations_view.py
@@ -143,7 +143,7 @@ def get_recommendations_based_off_interests():
     boundary = 0
     recommended_users = {}
 
-    other_users = User.query_order_by(func.random()).limit(150).all()
+    other_users = User.query.order_by(func.random()).limit(150).all()
 
     for u in other_users:
         common_interests = set(user.interests) & set(u.interests)
